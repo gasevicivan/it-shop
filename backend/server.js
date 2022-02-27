b283cc7442308ccbc8c17e555307d6c748640ff1
gasevicivan <gasevic_ivan@yahoo.com>
2022-02-27 12:09:20 +0100

    deploy

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,9 @@ app.get('/api/config/paypal', (req, res) =>{
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.send('Server is ready');
-});
+});*/
 app.use((err, req, res) => {
     res.status(500).send({message: err.message});
 });
