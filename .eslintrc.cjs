7f9309bdb4d90db6ebc1a0cca188b6e4bfbae96d
gasevicivan <gasevic_ivan@yahoo.com>
2022-02-27 20:22:49 +0100

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,7 @@ module.exports = {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 13,
+        "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "plugins": [
@@ -20,4 +20,4 @@ module.exports = {
     ],
     "rules": {
     }
-};
+}
